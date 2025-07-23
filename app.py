e7e699335992887651d3dca21a26aca38751c15b
Halston-droid <henryalston990@gmail.com>
2025-07-23 10:38:41 -0400

    Delete task.json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +20,7 @@ def visual():
 
 
 if __name__ == '__main__':      
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('FLASK_PORT', 5000), debug=os.environ.get('FLASK_DEBUG', True))
+
 
  
