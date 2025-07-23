199ae3f5ef71aaf9ed9c4c7a176bba6296b03030
Halston-droid <henryalston990@gmail.com>
2025-07-23 11:48:13 -0400

    changed greet.html to newCustomer.html and changed visual.html to newInvoice.html

--- app.py
+++ app.py
@@ -6,17 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')    
 
-@app.route('/greet', methods=['POST'])
-def greet():
-    name = request.form['name'] 
-    location = request.form['location']
-    amount = request.form['amount']
-    return render_template('greet.html', name=name, location=location, amount=amount)   
+@app.route('/newCustomer', methods=['POST'])
+def newCustomer():
+    name = request.form['newCustomer'] 
+    return render_template('newCustomer.html', name=name)   
 
 @app.route('/visual', methods=['POST'])
-def visual():
+def newInvoice():
     visual = request.form['visual']
-    return render_template('visual.html', visual=visual)
+    return render_template('newInvoice.html', visual=visual)
 
 
 if __name__ == '__main__':      
